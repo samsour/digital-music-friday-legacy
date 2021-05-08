3eca7ac0707745ed438d89813c08ebae22fa070d
samsour <sam.sauer@hotmail.de>
2021-05-08 04:05:27 +0200

    feat(pages): add error page

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1 v-if="error.statusCode === 404">{{ error.message }}</h1>
+		<h1 v-else>An error occurred</h1>
+		<NuxtLink to="/">Home page</NuxtLink>
+	</div>
+</template>
+
+<script>
+export default {
+	layout: 'error',
+	props: {
+		error: {
+			type: Object,
+			default: () => ({ message: 'Page not found', statusCode: 404 })
+		}
+	}
+};
+</script>
