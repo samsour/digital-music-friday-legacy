cad8ba578dfee6acae44d5216084020f30b81466
samsour <sam.sauer@hotmail.de>
2021-08-28 05:09:50 +0200

    feat: add back button

---
+++ frontend/components/BackButton.vue
@@ -0,0 +1,13 @@
+<template>
+	<button @click="handleClick">Back to previous page</button>
+</template>
+
+<script>
+export default {
+	methods: {
+		handleClick() {
+			this.$router.go(-1);
+		},
+	},
+};
+</script>
