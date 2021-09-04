e120cda52da61afdffb83b1bd488f69900ddb931
samsour <sam.sauer@hotmail.de>
2021-09-04 02:12:10 +0200

    refactor(socket-io): disable auto connect

--- frontend/plugins/socket-io.js
+++ frontend/plugins/socket-io.js
@@ -1,4 +1,6 @@
 import io from 'socket.io-client';
-const socket = io(process.env.WEBSOCKET_URL);
+const socket = io(process.env.WEBSOCKET_URL, {
+	autoConnect: false,
+});
 
 export default socket;
