792ff1bbb81ba4bc13c6ad0ce9c81481e794b14c
samsour <sam.sauer@hotmail.de>
2021-09-04 02:13:18 +0200

    feat(authentication): save roomId from url before route change

--- frontend/middleware/authentication.js
+++ frontend/middleware/authentication.js
@@ -1,6 +1,7 @@
 export default function ({ store, redirect, from }) {
 	// If the user is not authenticated
 	if (!store.state.user?.isAuthenticated) {
+		localStorage.roomId = from.params.id;
 		return redirect(process.env.API_URL + '/authorize');
 	}
 }
